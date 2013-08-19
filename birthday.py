f9a36ea9c9dc84e20333f24d3c17546d5d1fc02e
Pablo Ambrosio <pandres@gmail.com>
2013-08-19 12:11:43 -0400

    Put code in main function for faster execution.

--- birthday.py
+++ birthday.py
@@ -2,34 +2,37 @@
 import sys
 from datetime import datetime, timedelta
 
-file = ".birthdays"
+FILE = ".birthdays"
 
-births = open(file, "r")
-birth = births.readline()
-while birth:
-    data = birth.rpartition(" bd")[0]
-    if data:
-        who, when_string = data.split("=")
-        today = datetime.today()
-        try:
-            when = datetime.strptime(when_string.strip(), "%d/%m/%y")
-        except Exception, e:
+def main(file):
+    births = open(file, "r")
+    birth = births.readline()
+    while birth:
+        data = birth.rpartition(" bd")[0]
+        if data:
+            who, when_string = data.split("=")
+            today = datetime.today()
             try:
-                when = datetime.strptime(when_string.strip(), "%d/%m/%Y")
+                when = datetime.strptime(when_string.strip(), "%d/%m/%y")
+            except Exception, e:
+                try:
+                    when = datetime.strptime(when_string.strip(), "%d/%m/%Y")
+                except Exception, e:
+                    print "bang! " + str(e)
+                    birth = births.readline()
+                    continue
+    
+            try:
+                diff = datetime(today.year, when.month, when.day) - datetime(today.year, today.month, today.day)
             except Exception, e:
                 print "bang! " + str(e)
                 birth = births.readline()
                 continue
+    
+            if timedelta(0) <= diff < timedelta(31):
+                print "%s cumple el %s" % (who, when_string,)
+    
+        birth = births.readline()
 
-        try:
-            diff = datetime(today.year, when.month, when.day) - datetime(today.year, today.month, today.day)
-        except Exception, e:
-            print "bang! " + str(e)
-            birth = births.readline()
-            continue
-
-        if timedelta(0) <= diff < timedelta(31):
-            print "%s cumple el %s" % (who, when_string,)
-
-    birth = births.readline()
-
+if __name__ == "__main__":
+    main(FILE)
